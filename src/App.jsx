722d6a61d559f570ca51871df33cdb60be3cfc7a
Yusuf David <yusufdavid411@gmail..com>
2024-10-24 09:23:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className="content" id="content">
+
+        <header>
+
+          <div className="navigation" id="navigation">
+
+            <div className="logo" id="logo">
+              <img src="" alt="" />
+              <div className="writeup">The Logo</div>
+            </div>
+
+            <nav>
+              <ul>
+                <li><a href="">Bible Reading Plan</a></li>
+                <li><a href="">Assignments</a></li>
+                <li><a href="">Personal Study Notes</a></li>
+                <li><a href="">Personal Study</a></li>
+              </ul>
+            </nav>
+
+          </div>
+
+          <p className="vision" id="vision">The Vision Notes Study Notes Study Notes Study Notes Study Notes</p>
+
+        </header>
+
+        <main>
+          
+        </main>
+
+        <section>
+        </section>
+
+        <footer>
+
+          <div className="social" id="social">
+            <div className="icon"></div>
+            <div className="icon"></div>
+            <div className="icon"></div>
+            <div className="icon"></div>
+          </div>
+
+          <div className="map" id="map">
+            <img src="" alt="map" />
+          </div>
+
+          <p>The Vision Notes Study Notes Study Notes Study Notes Study Notes</p>
+
+        </footer>
+
+      </div>
+    </>
+  )
+  
+}
+
+export default App
