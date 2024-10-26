d2ab3e2d342b1cd7e79644815c0d5df196ca626a
Yusuf David <yusufdavid411@gmail..com>
2024-10-26 18:49:30 +0100

    new commit

--- src/Components/Header/Logo.jsx
+++ src/Components/Header/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = () => {
   return (
     <div className="logo" id="logo">
-      <img src='/img/spiritlife-logo.jfif' alt="logo" />
+      <img src='/img/spiritlife-logo5.jfif' alt="logo" />
     </div>
   )
 }
