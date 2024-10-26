f4984b9c5578856ca315740b4dac9843b661a992
Yusuf David <yusufdavid411@gmail..com>
2024-10-26 13:53:53 +0100

    new commit

--- src/Components/Header/Logo.jsx
+++ src/Components/Header/Logo.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Logo = () => {
   return (
     <div className="logo" id="logo">
-      <img src='/' alt="" />
-      <div className="writeup">The Logo</div>
+      <img src='flyer.jpg' alt="logo" />
+      <div className="writeup">Spirit life</div>
     </div>
   )
 }
