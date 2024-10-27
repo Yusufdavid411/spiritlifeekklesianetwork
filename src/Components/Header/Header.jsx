faebffaaf127b589095b5a558838a6c01cf8cec8
Yusuf David <yusufdavid411@gmail..com>
2024-10-27 14:01:17 +0100

    VISION STATEMENT

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -15,10 +15,13 @@ const Header = () => {
 
       </div>
 
-      <p className="vision" id="vision"><span>VISION STATEMENT:</span> A Ministry with a vision to be, and raise men in whom God can entrust his counsel in all sphere of life as ordained and not suffer lost</p>
+      <div className="vision" id="vision">
+        <span>VISION STATEMENT :</span>
+        <marquee behavior="" direction="left">A Ministry with a vision to be, and raise men in whom God can entrust his counsel in all sphere of life as ordained and not suffer lost</marquee>
+      </div>
 
     </header>
   );
 }
- 
+
 export default Header;
