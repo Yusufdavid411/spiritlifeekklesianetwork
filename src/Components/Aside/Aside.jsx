54b7775e9d79ea97701300160169b5f43dcdd371
Yusuf David <yusufdavid411@gmail..com>
2024-11-02 14:10:17 +0100

    anchorTag.add

--- src/Components/Aside/Aside.jsx
+++ src/Components/Aside/Aside.jsx
@@ -3,27 +3,27 @@ import { useState } from 'react';
 import React from 'react'
 import './aside-style.css'
 import ReactPlayer from 'react-player'
-import toast, { Toaster } from 'react-hot-toast';
+// import toast, { Toaster } from 'react-hot-toast';
 
 
 const items = [
-  {id: 1, title: 'item #4', img: (<img src='/img/img5.jpeg' alt="logo" />)},
-  {id: 2, title: 'item #1', img: (<img src='/img/img1.jfif' alt="logo" />)},
-  {id: 3, title: 'item #3', img: (<img src='/img/img3.jfif' alt="logo" />)},
+  {id: 1, title: 'item #4', img: (<a href="https://t.me/c/1182400329/2961"><img src='/img/img5.jpeg' alt="logo" /></a>)},
+  {id: 2, title: 'item #1', img: (<a href="https://t.me/c/1182400329/2961"><img src='/img/img1.jfif' alt="logo" /></a>)},
+  {id: 3, title: 'item #3', img: (<a href="https://t.me/c/1182400329/2961"><img src='/img/img2.jfif' alt="logo" /></a>)},
   {id: 4, title: 'item #2', img: (<ReactPlayer className="video" url='https://youtube.com/shorts/APUm_UaDjqg?si=Zozt1010PlQ5KIXr' />)},
-  {id: 5, title: 'item #4', img: (<img src='/img/img4.jpg' alt="logo" />)},
-  {id: 6, title: 'item #2', img: (<ReactPlayer className="video" url='https://t.me/c/1182400329/2955' />)},
+  {id: 5, title: 'item #4', img: (<a href="https://t.me/c/1182400329/2961"><img src='/img/img3.jfif' alt="logo" /></a>)},
+  {id: 6, title: 'item #2', img: (<a href="https://t.me/c/1182400329/2961"><img src='/img/img4.jpg' alt="logo" /></a>)},
 ];
 
 
 const Aside = () => {
 
-  const notify = () => toast('Here is your toast.');
+  // const notify = () => toast('');
   const [activeSlideIndex, setActiveSlideIndex] = useState(0);
 
   return (
     <aside>
-      <Toaster/>
+      {/* <Toaster/> */}
 
       <ReactSimplyCarousel
         activeSlideIndex={activeSlideIndex}
@@ -79,7 +79,7 @@ const Aside = () => {
         easing="linear"
       >
 
-        {items.map(item => <div onClick={notify} className="items" key={item.id}>{item.img}</div>)}
+        {items.map(item => <div className="items" key={item.id}>{item.img}</div>)}
 
       </ReactSimplyCarousel>
 
