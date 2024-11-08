0bb14f6384de2ee1e947091012a9163a85d47b2d
Yusuf David <yusufdavid411@gmail..com>
2024-11-08 13:34:21 +0100

    footer.add

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,31 +10,36 @@ const Footer = () => {
   return (
     <footer>
 
-      <div className="social" id="social">
+      <div className="container" id="container">
 
-        <a href="https://youtube.com/@judezakwoyi?si=scCwEitMSw9Dc7iA" className="icon">
-          <FontAwesomeIcon icon={faYoutube} />
-        </a>
-        <a href="https://www.facebook.com/judejerryzakwoyi/" className="icon">
-          <FontAwesomeIcon icon={faFacebook} />
-        </a>
-        <a href="https://t.me/SpiritLifemessages" className="icon">
-          <FontAwesomeIcon icon={faTelegram} />
-        </a>
-        <a href="https://www.instagram.com/jude_zakwoyi?igsh=dnhoMnd3Y3IybDM3" className="icon">
-          <FontAwesomeIcon icon={faInstagram} />
-        </a>
+        <div className="social" id="social">
+
+          <a href="https://youtube.com/@judezakwoyi?si=scCwEitMSw9Dc7iA" className="icon">
+            <FontAwesomeIcon icon={faYoutube} />
+          </a>
+          <a href="https://www.facebook.com/judejerryzakwoyi/" className="icon">
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+          <a href="https://t.me/SpiritLifemessages" className="icon">
+            <FontAwesomeIcon icon={faTelegram} />
+          </a>
+          <a href="https://www.instagram.com/jude_zakwoyi?igsh=dnhoMnd3Y3IybDM3" className="icon">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+
+        </div>
+
+        <div className="map" id="map">
+          <a href='https://maps.app.goo.gl/D7TuiLhUn7ig83d46'>
+            <FontAwesomeIcon icon={faLocationDot} rotation={180} bounce style={{color: "red"}} />
+          </a>
+        </div>
+
+        <p>SPIRITLIFE EKKLESIA NETWORK, YUV ACADEMY CHIKA, PHASE 3, ALONG AIRPORT ROAD ABUJA</p>
+
 
       </div>
-
-      <div className="map" id="map">
-        <a href='https://maps.app.goo.gl/D7TuiLhUn7ig83d46'>
-          <FontAwesomeIcon icon={faLocationDot} rotation={180} bounce style={{color: "red"}} />
-        </a>
-      </div>
-
-      <p>SPIRITLIFE EKKLESIA NETWORK, YUV ACADEMY CHIKA, PHASE 3, ALONG AIRPORT ROAD ABUJA</p>
-
+      
     </footer>
   )
 }
