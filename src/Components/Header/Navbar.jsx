979f34a94e0670ccb9f47d69ee46c51607a574f0
David <yusufdavid411@gmail.com>
2025-02-03 18:48:00 +0100

    spheres updated

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -51,7 +51,30 @@ const Navbar = () => {
           
         </li>
 
-        <li>Departments</li>
+        <li onClick={() => handleItemClick(2, 'left')}>
+
+          Departments
+
+          {activeItem === 2 && (
+            <div className={`link-options ${linkPosition}`}>
+              <div className="link-optional">
+                <a href="/" target="_blank" rel="noopener noreferrer" className="link-option">Zoe Streams <br /> (Music Department)</a>
+                <a href="/" target="_blank" rel="noopener noreferrer" className="link-option">Kingdom Parables <br /> (Drama Department)</a>
+                <a href="/" target="_blank" rel="noopener noreferrer" className="link-option">The </a>
+                <a href="/" target="_blank" rel="noopener noreferrer" className="link-option"></a>
+                <a href="/" target="_blank" rel="noopener noreferrer" className="link-option"></a>
+                <a href="/" target="_blank" rel="noopener noreferrer" className="link-option"></a>
+                <a href="/" target="_blank" rel="noopener noreferrer" className="link-option"></a>
+                <a href="/" target="_blank" rel="noopener noreferrer" className="link-option"></a>
+                <a href="/" target="_blank" rel="noopener noreferrer" className="link-option"></a>
+              </div>
+
+              <div><FontAwesomeIcon icon={faCaretRight} className="option-icon-btn" /></div>
+            </div>
+          )}
+          
+        </li>
+
         <li>Personal Study Notes</li>
       </ul>
 
