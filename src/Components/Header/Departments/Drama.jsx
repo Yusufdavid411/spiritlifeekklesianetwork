124a6ab7679cdda8e97c6b5e2292bf02d41878ef
David <yusufdavid411@gmail.com>
2025-03-11 02:37:39 +0100

    meta properties

--- src/Components/Header/Departments/Drama.jsx
+++ src/Components/Header/Departments/Drama.jsx
@@ -1,6 +1,6 @@
 import './drama.css'
 import React, { useState } from "react";
-import ReactPlayer from "react-player/youtube";
+import ReactPlayer from 'react-player';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from "react-router-dom"
@@ -10,7 +10,7 @@ const Aside = () => {
   const navigate = useNavigate();
 
   return (
-    <div style={{ position: "relative", width: "100%", height: "80vh", overflow: "hidden" }}>
+    <div style={{ width: "100%", height: "100vh", overflow: "hidden" }}>
       
       <span
         onClick={() => navigate("/")}
@@ -25,15 +25,22 @@ const Aside = () => {
         // className="video"
         playing={true}
         loop={true}
+        controls={true}
         width="100%"
         height="100%"
-        style={{ position: "absolute", top: 0, left: 0,}}
+        style={{ top: 0, left: 0,}}
       />
 
+      {/* <ReactPlayer
+        className="video"
+        style={{ top: 0, left: 0, zIndex: 1,}}
+        url='https://youtube.com/shorts/APUm_UaDjqg?si=Zozt1010PlQ5KIXr'
+      /> */}
+
       {/* Content Above the Video */}
-      <div style={{ position: "relative", fontSize: "1.5rem", zIndex: 1, color: "white", textAlign: "center", marginTop: "10%" }}>
+      {/* <div style={{ position: "relative", fontSize: "1.5rem", zIndex: 20, color: "white", textAlign: "center", marginTop: "10%" }}>
         <p>Kingdom Parables <br /> (Drama Department)</p>
-      </div>
+      </div> */}
     </div>
   );
 };
